7624508d74e308159de2765f3f2392bfe7544902
Đạt Nguyễn Duy <81822483+DatNguyen2711@users.noreply.github.com>
2023-07-04 09:40:44 +0700

    Update

--- src/Components/APINET7/Deletebtn.js
+++ src/Components/APINET7/Deletebtn.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "@mui/material";
 import axios from "axios";
+import { Popconfirm } from "antd";
 
 const Deletebtn = ({ productId, setProductData }) => {
+  const [confirmVisible, setConfirmVisible] = useState(false);
   const handleDelete = () => {
+    setConfirmVisible(true); 
+
+  };
+
+  const handleConfirmDelete = () => {
     axios
       .delete(`${process.env.REACT_APP_LINK}/${productId}`)
-
       .then((response) => {
         if (response.status === 200) {
           setProductData((prev) =>
@@ -14,13 +20,29 @@ const Deletebtn = ({ productId, setProductData }) => {
           );
         }
       })
-      .catch((error) => {});
+      .catch((error) => {
+        // Xử lý lỗi (nếu cần)
+      });
+
+    setConfirmVisible(false); // Ẩn Popconfirm
   };
+
   return (
     <div>
       <Button variant="contained" color="error" onClick={handleDelete}>
         Delete
       </Button>
+
+      <Popconfirm
+        onConfirm={handleConfirmDelete}
+        onCancel={() => setConfirmVisible(false)}
+        title="Delete the hero"
+        description="Are you sure to delete this Hero?"
+        okText="Yes"
+        cancelText="No"
+      >
+        <Button danger>Delete</Button>
+      </Popconfirm>
     </div>
   );
 };
