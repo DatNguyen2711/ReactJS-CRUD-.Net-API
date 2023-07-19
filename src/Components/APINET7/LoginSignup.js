0790aecbca4d59afa1e551292421e62414ad6a63
Đạt Nguyễn Duy <81822483+DatNguyen2711@users.noreply.github.com>
2023-07-19 11:44:53 +0700

    Xong signout khi đăng nhập google

---
+++ src/Components/APINET7/LoginSignup.js
@@ -0,0 +1,121 @@
+import { useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+} from "firebase/auth";
+import { auth, signInWithGoogle } from "./firebase-config";
+
+function App() {
+  const [registerEmail, setRegisterEmail] = useState("");
+  const [registerPassword, setRegisterPassword] = useState("");
+  const [loginEmail, setLoginEmail] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+
+  const [user, setUser] = useState({});
+
+  onAuthStateChanged(auth, (currentUser) => {
+    setUser(currentUser);
+  });
+
+  const register = async () => {
+    try {
+      const user = await createUserWithEmailAndPassword(
+        auth,
+        registerEmail,
+        registerPassword
+      );
+      console.log(user);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  const login = async () => {
+    try {
+      const user = await signInWithEmailAndPassword(
+        auth,
+        loginEmail,
+        loginPassword
+      );
+      console.log(user);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  const logout = async () => {
+    try {
+      await signOut(auth);
+      localStorage.removeItem("name");
+      localStorage.removeItem("photo");
+      localStorage.removeItem("email");
+      setUser({});
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+  return (
+    <div className="App">
+      {/* <div>
+        <h3> Register User </h3>
+        <input
+          placeholder="Email..."
+          onChange={(event) => {
+            setRegisterEmail(event.target.value);
+          }}
+        />
+        <input
+          placeholder="Password..."
+          onChange={(event) => {
+            setRegisterPassword(event.target.value);
+          }}
+        />
+
+        <button onClick={register}> Create User</button>
+      </div>
+
+      <div>
+        <h3> Login </h3>
+        <input
+          placeholder="Email..."
+          onChange={(event) => {
+            setLoginEmail(event.target.value);
+          }}
+        />
+        <input
+          placeholder="Password..."
+          onChange={(event) => {
+            setLoginPassword(event.target.value);
+          }}
+        />
+
+        <button onClick={login}> Login</button>
+      </div>
+
+      <h4> User Logged In: </h4>
+      {user?.email}
+
+      <button onClick={logout}> Sign Out </button> */}
+
+      <button
+        onClick={signInWithGoogle}
+        type=""
+        style={{ backgroundColor: "gray", marginLeft: "50%" }}
+      >
+        {" "}
+        sign with google
+      </button>
+      <button type="" onClick={logout}>
+        {" "}
+        sign out
+      </button>
+      <h1>{localStorage.getItem("name")}</h1>
+      <h1>{localStorage.getItem("email")}</h1>
+      <img src={localStorage.getItem("photo")} alt="" />
+    </div>
+  );
+}
+
+export default App;
